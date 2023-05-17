d6afe83f05c41b613fa0b158b413155320a6adca
Qusai Issa <qusai.2010.123@hotmail.com>
2023-05-17 21:16:01 +0300

    Mobile ver ready

---
+++ details.js
@@ -0,0 +1,9 @@
+const darkBtn = document.getElementById('dark-mode-btn');
+darkBtn.onclick = () => {
+  document.body.classList.toggle('dark-theme');
+};
+
+
+const openMainPage=() =>{
+    document.location.href ='index.html';
+  }
